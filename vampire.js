20740e237f824668802620c92dc28c66ddc365e9
Stoneface-Vimes <cliffweir92@gmail.com>
2020-01-24 17:43:33 -0500

    Completed all the basic functionality of the vampyr app, minus the stretch feature

--- vampire.js
+++ vampire.js
@@ -10,22 +10,39 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
+    this.offspring.push(vampire);
+    vampire.creator = this;
 
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-
+    let count = 0; //Ah ah ah
+    let currentVampire = this;
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      count++;
+    }
+    return count;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    let currentVampire = this;
+    while (currentVampire.creator && vampire.creator) {
+      currentVampire = currentVampire.creator;
+      vampire = vampire.creator
+    }
+    if (!currentVampire.creator && vampire.creator) {
+      return true;
+    }  else if (currentVampire.creator && !vampire.creator){
+      return false;
+    }
   }
 
   /** Stretch **/
